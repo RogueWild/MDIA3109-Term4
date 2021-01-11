12e89812ae317aa130f5b59436df6160a2e536aa
RogueWild <0.dreamer.009@gmail.com>
2021-01-11 16:14:02 +0300

    practice 1 comp finished

--- src/comps/Practices/Practice2/Table/index.js
+++ src/comps/Practices/Practice2/Table/index.js
@@ -3,19 +3,98 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     position:relative;
-    max-width:400px;
-    min-height:330px;
+    max-width:300px;
+    min-height:auto;
+    background:#F7F7F7;
     display:flex;
-    justify-content:top;
+`;
+
+const Header = styled.div`
+    position:relative;
+    background:#5F83FF;
+    color:white;
+    font-weight:500;
+    max-width:300px;
+    min-height:50px;
+    display:flex;
     align-items:center;
+
+    & > span {
+        margin-left:10px;
+    }
+`;
+
+const LeftCont = styled.div`
+    position:relative;
+    width:50%;
+    height:250px;
+    display:flex;
     flex-direction:column;
-    padding:20px 0px;
+    justify-content:space-evenly;
+    text-align:left;
+
+    & > span {
+        margin-left:10px;
+    }
+`;
+
+const RightCont = styled.div`
+    position:relative;
+    width:50%;
+    height:250px;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-evenly;
+    text-align:right;
+    color:#777777;
+
+    & > span {
+        margin-right:10px;
+    }
 `;
 
 const Table = ({ }) => {
-    return <Container>
-
-    </Container>
+    return <div>
+        <Header>
+            <span>exercise</span>
+        </Header>
+        <Container>
+            <LeftCont>
+                <span>
+                    <b>id</b>
+                </span>
+                <span>
+                    name
+            </span>
+                <span>
+                    reps
+            </span>
+                <span>
+                    calories
+            </span>
+                <span>
+                    created
+            </span>
+            </LeftCont>
+            <RightCont>
+                <span>
+                    <b>int</b>
+                </span>
+                <span>
+                    text
+            </span>
+                <span>
+                    int
+            </span>
+                <span>
+                    int
+            </span>
+                <span>
+                    timestamp
+            </span>
+            </RightCont>
+        </Container>
+    </div>
 }
 
 Table.defaultProps = {
--- src/pages/Practices/Practice2/index.js
+++ src/pages/Practices/Practice2/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Table from '../../../comps/Practices/Practice2/Table';
 
-const Practice1 = () => {
+const Practice2 = () => {
     return <div className="practice2">
         <Table />
     </div>
 }
 
-export default Practice1;
+export default Practice2;
